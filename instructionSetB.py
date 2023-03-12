ec9ab0a58e8249a3c34c05666460a3a1e36c9db8
Simon <104141356+SimonBaum@users.noreply.github.com>
2023-03-12 22:56:29 +0100

    Update instructionSetB.py

--- instructionSetB.py
+++ instructionSetB.py
@@ -21,19 +21,3 @@ os.system(r"magick composite -gravity NorthWest -geometry +20+5 C:\Users\K017-La
 #shutil.copy2(source, destination, new_file_name)
 
 os.system("echo Your picture is ready to print now!")
-
- #   result_name = filename2
-  #  print result_name
-   # i = 0
-
-    # Check if file exists
-    #if arcpy.Exists(draftloc + "\\" + result_name):
-        # If it does, increment i by 1
-     #   i+=1
-        # While each successive filename (including i) does not exists, then save the next filename
-       # while not arcpy.Exists(draftloc + "\\" + shortname + "_" + str(i) + extension):                
-      #      mxd.saveACopy(draftloc + "\\" + shortname + "_" + str(i) + extension)            
-    # else if the original file didn't satisfy the if, the save it.
-    #else:           
-     #   mxd.saveACopy(draftloc + "\\" + result_name)
-
